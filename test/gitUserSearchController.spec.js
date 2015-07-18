07d3e2dd2ccb6a9db3fb47ac81dfecf598a5d847
Sam Joseph <tansaku@gmail.com>
2015-07-18 21:53:40 +0100

    adds test for search result and hard codes pass

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -11,4 +11,23 @@ describe('GitUserSearchController', function() {
     expect(controller.searchResult).toBeUndefined(); // TypeError: 'undefined' is not an object (evaluating 'controller.searchResult')
     expect(controller.searchTerm  ).toBeUndefined();
   });
+
+  describe('when searching for a user',function(){
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      }, 
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function(){
+      expect(controller.searchResults.items).toEqual(items);
+    });
+  });
 });
